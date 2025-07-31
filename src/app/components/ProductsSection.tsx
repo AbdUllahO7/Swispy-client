6184436a5964277767e344db06eb5cd76b6639c5
AJSABBAH <abood_2000_@live.com>
2025-07-31 15:37:40 +0300

    noe update

--- src/app/components/ProductsSection.tsx
+++ src/app/components/ProductsSection.tsx
@@ -28,7 +28,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ title, description, icon, ico
             <img 
               src={icon}
               alt={iconAlt}
-              className="w-24 h-24 md:w-32 md:h-32 object-contain"
+              className="w-224 h-24  object-contain"
               onError={(e) => {
                 console.log('Image failed to load:', icon);
                 // Show a placeholder or fallback
@@ -80,15 +80,23 @@ const ProductsSection: React.FC = () => {
     {
       title: t('products.flirtBox.title'),
       description: t('products.flirtBox.description'),
-      icon: '/assets/chs.png', // Use full absolute URL
+      icon: '/assets/hah.jpeg', // Use full absolute URL
       iconAlt: 'Flirt Box Icon',
       color: 'from-pink-300 to-rose-400'
     },
       
     {
-      title: t('products.secrit.title'),
-      description: t('products.flirtBox.description'),
-      icon: '/assets/header.png', // Use full absolute URL
+      title: t('products.SecretBox.title'),
+      description: t('products.SecretBox.description'),
+      icon: '/assets/love.png', // Use full absolute URL
+      iconAlt: 'Flirt Box Icon',
+      color: 'from-pink-300 to-rose-400'
+    },
+    
+    {
+      title: t('products.SignatureBox.title'),
+      description: t('products.SignatureBox.description'),
+      icon: '/assets/ahaaa.png', // Use full absolute URL
       iconAlt: 'Flirt Box Icon',
       color: 'from-pink-300 to-rose-400'
     },
